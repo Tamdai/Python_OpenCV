b3b2b61ef092df6d2a36580e595c931d954db704
Ben_Aphirat <35913424+Tamdai@users.noreply.github.com>
2022-12-15 22:02:02 +0700

    Update basic3_resize.py

--- basic3_resize.py
+++ basic3_resize.py
@@ -2,7 +2,6 @@ import cv2
 img = cv2.imread("image/cat.jpg") 
 imgresize = cv2.resize(img,(400,400)) 
 
-#แสดงภาพ
 cv2.imshow("Output",imgresize) 
 cv2.waitKey(delay=5000) 
 cv2.destroyAllWindows() 
