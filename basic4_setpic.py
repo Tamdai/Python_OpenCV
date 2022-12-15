2910b05cdf7cf119e96b26c924470c1e48e3d9a5
Ben_Aphirat <35913424+Tamdai@users.noreply.github.com>
2022-12-15 22:02:43 +0700

    Update basic4_setpic.py

--- basic4_setpic.py
+++ basic4_setpic.py
@@ -1,11 +1,7 @@
-#กำหนดรูปแบบ
+import cv2 
+img = cv2.imread("image/cat.jpg",0) 
+imgresize = cv2.resize(img,(400,400)) 
 
-#อ่านภาพมาทำงาน
-import cv2 #เรียกใช้งานคำสั่ง
-img = cv2.imread("image/cat.jpg",0) #**ใส่ค่าหลัง("image/cat.jpg",?)" 0 = Gray 1 = color -1 = unfar cinal
-imgresize = cv2.resize(img,(400,400)) #ตัวแปร+กำหนดขนาด
-
-#แสดงภาพ
-cv2.imshow("Output",imgresize) #tital+pic
-cv2.waitKey(delay=5000) #หน่วงเวลา
-cv2.destroyAllWindows() #คืนค่า
+cv2.imshow("Output",imgresize) 
+cv2.waitKey(delay=5000) 
+cv2.destroyAllWindows() 
