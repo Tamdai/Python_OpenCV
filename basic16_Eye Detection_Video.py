f55b81a564435b0f50f4765d5b17780c8357a2f5
Ben_Aphirat <35913424+Tamdai@users.noreply.github.com>
2022-12-15 22:29:39 +0700

    Update basic16_Eye Detection_Video.py

--- basic16_Eye Detection_Video.py
+++ basic16_Eye Detection_Video.py
@@ -1,18 +1,17 @@
 import cv2 
-#อ่านวิดีโอ
+
 cap = cv2.VideoCapture("image/Mark.mp4")
 
-#อ่านไฟล์สำหรับ classification ไฟล์HTML เข้ามาทำงาน
+#classification ไฟล์HTML เข้ามาทำงาน
 eye_cascade=cv2.CascadeClassifier("Detect/haarcascade_eye_tree_eyeglasses.xml")
 
-#แสดงผลวิดีโอ
 while (cap.isOpened()):
     check , frame = cap.read()
     if check == True :
-        gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #ภาพสีกลายเป็น gray
-        #จำแนกใบหน้า
+        gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) 
+      
         eye_detect = eye_cascade.detectMultiScale(gray_img,1.2,5)
-        #แสดงตำแหน่งที่เจอใบหน้า
+  
         for (x,y,w,h) in eye_detect:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),thickness=5)
             cv2.imshow("Output",frame)
