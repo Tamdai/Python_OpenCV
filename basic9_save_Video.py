1b1e84a6c61a332b4c9030b3a93c9057a6a8904e
Ben_Aphirat <35913424+Tamdai@users.noreply.github.com>
2022-12-15 22:13:12 +0700

    Update basic9_save_Video.py

--- basic9_save_Video.py
+++ basic9_save_Video.py
@@ -1,20 +1,19 @@
-#Save Video
 import cv2
 
-cap = cv2.VideoCapture(0) #อ่านvideoกล้อง ()จำนวนกล้อง
-fourcc = cv2.VideoWriter_fourcc(*'XVID') #
+cap = cv2.VideoCapture(0) 
+fourcc = cv2.VideoWriter_fourcc(*'XVID') 
 
-result = cv2.VideoWriter("output.avi",fourcc,20.0,(640,480)) #ไฟล์ที่ส่งมา บึนทึกอะไรบ้าง
+result = cv2.VideoWriter("output.avi",fourcc,20.0,(640,480)) 
 
-while (cap.isOpened()): #ตรวจสอบ loop สามารถใช้งานได้ ผ่านตัวแปร + ฟังชั่น  อ่านได้ ทำงานได้
-    check , fraem = cap.read() #รับภาพจากกล้อง Frem : Frem ตัวแปร
+while (cap.isOpened()): 
+    check , fraem = cap.read() 
+    
+    if check == True : 
+        cv2.imshow("Output",fraem)
+        result.write(fraem) 
+        if cv2.waitKey(1) & 0xFF == ord("e"): 
+            break 
 
-    if check == True : #เช็คก่อนว่าวีดีโอยังเล่นไม่จบ
-        cv2.imshow("Output",fraem) #แสดงผลออกมา แต่ละเฟรม
-        result.write(fraem) #นำเอาค่าแดต่ละเฟรมเขียนลงไป
-        if cv2.waitKey(1) & 0xFF == ord("e"): #(กดตัว e = ตัดจบ) หยุดถ่ายvideo
-            break #out loop
-
-result.release() #รวบรวมไฟล์
-cap.release() #เคลีย
+result.release() 
+cap.release() 
 cv2.destroyAllWindows()
